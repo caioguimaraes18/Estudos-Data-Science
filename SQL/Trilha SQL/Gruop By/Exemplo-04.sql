7e0218e5012b1f267f773a3f4940905c3208071e
Caio Guimarães <contatocaioguimaraess@gmail.com>
2024-10-16 21:20:41 -0300

    Fix: Atualização FIAP

---
+++ SQL/Trilha SQL/Gruop By/Exemplo-04.sql
@@ -0,0 +1,7 @@
+-- Quantos produtos de 'beleza_saude' tem mais de 1 litros?
+
+select 
+    count(*) as qtd_produtos
+from tb_products
+where product_length_cm * product_height_cm * product_width_cm / 1000 < 1
+and product_category_name = 'beleza_saude'
